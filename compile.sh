866efc79250d0ec65002ca10ed30196a000cac88
BionicPotato <73661894+BionicPotato@users.noreply.github.com>
2024-10-02 08:28:59 -0400

    stop my own shell scripts from being tracked
    I use them to compile plec but the "official" plec workflow should just
    be cmake and nothing more

--- compile.sh
+++
@@ -1,1 +0,0 @@
-cd /home/catardif/plec/core.bin && make && sudo make install && cd ../plec.bin && make && sudo make install && cd ..
--- mkcorebin.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-mkdir core.bin
-cd core.bin
-cmake ../core -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Release
-cd ..
-

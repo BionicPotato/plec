1534dbd88719fe332ee66becb973c3b3694f239f
BionicPotato <73661894+BionicPotato@users.noreply.github.com>
2024-11-06 09:38:18 -0500

    Fix crashing on parsing errors

--- core/statementstream.cpp
+++ core/statementstream.cpp
@@ -37,108 +37,115 @@ shared_ptr<Expression> StatementStream::getExpression(const Token& start, TokenI
     list<list<ParsingExpression>::iterator> colons;
     list<list<ParsingExpression>::iterator> commas;
 
-    for (; tok.id != end; getNextToken(tok))
+    try
     {
-        switch (tok.id)
+        for (; tok.id != end; getNextToken(tok))
         {
-            case TOK_OPENCURLYBR: {
-                shared_ptr<BlockExpr> blockp = make_shared<BlockExpr>(tok);
-                exprs.emplace_back(tok, blockp, nullptr);
-                BlockStmtStream blockss(lexp);
-                shared_ptr<Statement> blockstp;
-                while (blockss.getNextStatement(blockstp))
-                    blockp->statements.push_back(blockstp);
-            }
-            break;
-
-            case TOK_OPENSQBR: {
-                shared_ptr<Expression> exprp;
-                if (!getNextToken(tok)) throw UnexpectedTokenException(tok);
-                exprp = getExpression(tok, TOK_CLOSESQBR);
-                shared_ptr<ArrayExpr> arrayp;
-                const Expression* _e = exprp.get();
-                const type_info& t_e = typeid(*_e);
-                const type_info& t_ae = typeid(ArrayExpr);
-                if (t_e == t_ae) {
-                    arrayp = static_pointer_cast<ArrayExpr>(exprp);
-                } else {
-                    arrayp = make_shared<ArrayExpr>(tok);
-                    arrayp->expressions.push_back(exprp);
+            switch (tok.id)
+            {
+                case TOK_OPENCURLYBR: {
+                    shared_ptr<BlockExpr> blockp = make_shared<BlockExpr>(tok);
+                    exprs.emplace_back(tok, blockp, nullptr);
+                    BlockStmtStream blockss(lexp);
+                    shared_ptr<Statement> blockstp;
+                    while (blockss.getNextStatement(blockstp))
+                        blockp->statements.push_back(blockstp);
                 }
-                exprs.emplace_back(tok, arrayp, nullptr);
+                break;
+
+                case TOK_OPENSQBR: {
+                    shared_ptr<Expression> exprp;
+                    if (!getNextToken(tok)) throw UnexpectedTokenException(tok);
+                    exprp = getExpression(tok, TOK_CLOSESQBR);
+                    shared_ptr<ArrayExpr> arrayp;
+                    const Expression* _e = exprp.get();
+                    const type_info& t_e = typeid(*_e);
+                    const type_info& t_ae = typeid(ArrayExpr);
+                    if (t_e == t_ae) {
+                        arrayp = static_pointer_cast<ArrayExpr>(exprp);
+                    } else {
+                        arrayp = make_shared<ArrayExpr>(tok);
+                        arrayp->expressions.push_back(exprp);
+                    }
+                    exprs.emplace_back(tok, arrayp, nullptr);
+                }
+                break;
+
+                case TOK_OPENPAREN: {
+                    shared_ptr<Expression> parenexprp;
+                    parenexprp = getExpression(tok, TOK_CLOSEPAREN);
+                    exprs.emplace_back(parenexprp->token, parenexprp, nullptr);
+                }
+                break;
+                
+                case TOK_IDENTIFIER:
+                    exprs.emplace_back(tok, shared_ptr<Expression>(nullptr), &identifiers);
+                    identifiers.push_back(prev(exprs.end()));
+                    exprs.back().it = prev(identifiers.end());
+                break;
+
+                case TOK_STRING:
+                    exprs.emplace_back(tok, shared_ptr<Expression>(nullptr), &strings);
+                    strings.push_back(prev(exprs.end()));
+                    exprs.back().it = prev(strings.end());
+                break;
+
+                case TOK_CALL:
+                    exprs.emplace_back(tok, shared_ptr<Expression>(nullptr), &calls);
+                    calls.push_back(prev(exprs.end()));
+                    exprs.back().it = prev(calls.end());
+                break;
+
+                case TOK_PLUS:
+                    exprs.emplace_back(tok, shared_ptr<Expression>(nullptr), &addsub);
+                    addsub.push_back(prev(exprs.end()));
+                    exprs.back().it = prev(addsub.end());
+                break;
+                
+                case TOK_COLON:
+                    exprs.emplace_back(tok, shared_ptr<Expression>(nullptr), &colons);
+                    colons.push_back(prev(exprs.end()));
+                    exprs.back().it = prev(colons.end());
+                break;
+
+                case TOK_COMMA:
+                    exprs.emplace_back(tok, shared_ptr<Expression>(nullptr), &commas);
+                    commas.push_back(prev(exprs.end()));
+                    exprs.back().it = prev(commas.end());
+                break;
+
+                case TOK_UNKNOWN:
+                    throw UnknownTokenException(tok);
+                break;
+
+                default:
+                    throw UnexpectedTokenException(tok);
+                break;
             }
-            break;
-
-            case TOK_OPENPAREN: {
-                shared_ptr<Expression> parenexprp;
-                parenexprp = getExpression(tok, TOK_CLOSEPAREN);
-                exprs.emplace_back(parenexprp->token, parenexprp, nullptr);
-            }
-            break;
-            
-            case TOK_IDENTIFIER:
-                exprs.emplace_back(tok, shared_ptr<Expression>(nullptr), &identifiers);
-                identifiers.push_back(prev(exprs.end()));
-                exprs.back().it = prev(identifiers.end());
-            break;
-
-            case TOK_STRING:
-                exprs.emplace_back(tok, shared_ptr<Expression>(nullptr), &strings);
-                strings.push_back(prev(exprs.end()));
-                exprs.back().it = prev(strings.end());
-            break;
-
-            case TOK_CALL:
-                exprs.emplace_back(tok, shared_ptr<Expression>(nullptr), &calls);
-                calls.push_back(prev(exprs.end()));
-                exprs.back().it = prev(calls.end());
-            break;
-
-            case TOK_PLUS:
-                exprs.emplace_back(tok, shared_ptr<Expression>(nullptr), &addsub);
-                addsub.push_back(prev(exprs.end()));
-                exprs.back().it = prev(addsub.end());
-            break;
-            
-            case TOK_COLON:
-                exprs.emplace_back(tok, shared_ptr<Expression>(nullptr), &colons);
-                colons.push_back(prev(exprs.end()));
-                exprs.back().it = prev(colons.end());
-            break;
-
-            case TOK_COMMA:
-                exprs.emplace_back(tok, shared_ptr<Expression>(nullptr), &commas);
-                commas.push_back(prev(exprs.end()));
-                exprs.back().it = prev(commas.end());
-            break;
-
-            case TOK_UNKNOWN:
-                throw UnknownTokenException(tok);
-            break;
-
-            default:
-                throw UnexpectedTokenException(tok);
-            break;
         }
+
+        parseVariables(exprs, identifiers);
+        parseStrings(exprs, strings);
+        parseDecl(exprs, identifiers);
+        parseCalls(exprs, calls);
+        parseAddSub(exprs, addsub);
+        parseVariableAssigns(exprs, colons);
+        parseCommas(exprs, commas);
+
+        if (exprs.size() != 1) {
+            shared_ptr<stack<shared_ptr<Expression>>> errorExprs;
+            for (ParsingExpression p : exprs) errorExprs->push(p.exprp);
+            throw AmbiguousStatementException(errorExprs);
+        }
+
+        shared_ptr<Expression> retval = exprs.front().exprp;
+        exprs.clear(); // Avoids calling ParsingExpression's destructor with an invalid iterator
+        return retval;
     }
-
-    parseVariables(exprs, identifiers);
-    parseStrings(exprs, strings);
-    parseDecl(exprs, identifiers);
-    parseCalls(exprs, calls);
-    parseAddSub(exprs, addsub);
-    parseVariableAssigns(exprs, colons);
-    parseCommas(exprs, commas);
-
-    if (exprs.size() != 1) {
-        shared_ptr<stack<shared_ptr<Expression>>> errorExprs;
-        for (ParsingExpression p : exprs) errorExprs->push(p.exprp);
-        throw AmbiguousStatementException(errorExprs);
+    catch (exception& e)
+    {
+        exprs.clear(); // Avoids calling ParsingExpression's destructor with an invalid iterator
+        throw;
     }
-
-    shared_ptr<Expression> retval = exprs.front().exprp;
-    exprs.clear(); // Avoids calling ParsingExpression's destructor with an invalid iterator
-    return retval;
-    // return exprs.front().exprp;
 }
 

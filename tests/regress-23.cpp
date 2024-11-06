115f85b99e25842fad995fea94ae327d53889cf9
BionicPotato <73661894+BionicPotato@users.noreply.github.com>
2024-11-06 11:18:50 -0500

    Add regression test for issue #23

---
+++ tests/regress-23.cpp
@@ -0,0 +1,25 @@
+#include "../core/ast/statement.hpp"
+#include "../core/stmtstreams/file.hpp"
+#include "../core/lexer.hpp"
+#include "../core/exceptions/ambiguousstatement.hpp"
+#include <memory>
+
+using namespace std;
+
+// The bug turned out to happen whenever an exception occurs during the execution of StatementStream::getExpression
+// So this test should test for getExpression causing a crash when throwing *any* exception
+// So it shouldn't actually rely on a TOK_NUMBER being present, because these will eventually get added to the language and not cause an error anymore
+// Ambiguous statements are a reliable way of causing a parsing error, so this is what this test is testing
+int main()
+{
+    shared_ptr<Lexer> lexp = make_shared<Lexer>("23-crash-when-encountering-a-number-token.ple");
+    FileStmtStream filess(lexp);
+    shared_ptr<Statement> stp;
+
+    try {
+        filess.getNextStatement(stp);
+    } catch (AmbiguousStatementException& e) {
+        return EXIT_SUCCESS;
+    }
+}
+
